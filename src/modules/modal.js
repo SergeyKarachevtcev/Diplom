190f12c8ab4b72875e4f184520e5cd0115a98257
SergeyKarachevtcev <estel141081@gmail.com>
2024-03-11 17:04:56 +0300

    main

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,16 +1,7 @@
 "use strict";
 
 const modal = () => {
-	const callbackBtn = document.getElementById("callbackBtn"); // кнопка коллбек
-	const modalOverlay = document.querySelector(".modal-overlay"); // подложка
-	const modalCallback = document.getElementById("modalCallback"); // модальное окно
-
-	// Назначаем обработчик события по клику
-	callbackBtn.addEventListener("click", function (event) {
-		// Ваше действие при клике на ссылку
-		console.log("Клик по ссылке произошел!");
-		modalOverlay.style.display = "block";
-	});
+	console.log("modal");
 };
 
 export default modal;
