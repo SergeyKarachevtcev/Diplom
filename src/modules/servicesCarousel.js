b43e3baab899a2f91bb295de0f3f54baac46b2fc
SergeyKarachevtcev <estel141081@gmail.com>
2024-03-14 15:51:24 +0300

    done

--- src/modules/servicesCarousel.js
+++ src/modules/servicesCarousel.js
@@ -13,15 +13,12 @@ const servicesCarousel = () => {
         breakpoints: {
             640: {
                 slidesPerView: 1,
-                /* spaceBetween: 20, */
             },
             768: {
                 slidesPerView: 2,
-                /* spaceBetween: 30, */
             },
             1024: {
                 slidesPerView: 3,
-                /* spaceBetween: 40, */
             },
         },
 
